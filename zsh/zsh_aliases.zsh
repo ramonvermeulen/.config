12b3796bb812fe5013817545f91455a5062165b8
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-12-05 15:04:02 +0100

    feat: update zsh aliases

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,10 +1,5 @@
 #!/usr/bin/env zsh
 
-#handy
-alias _ip="curl ifconfig.me"
-alias nvenv="python -m venv .venv"
-
-# neovim
 load_node_before_nvim() {
     if [ -s "$NVM_DIR/nvm.sh" ]; then
         source "$NVM_DIR/nvm.sh"
@@ -16,11 +11,10 @@ load_node_before_nvim() {
 alias vi=load_node_before_nvim
 alias vim=load_node_before_nvim
 alias nvim=load_node_before_nvim
-
-# os
 alias up="sudo apt-get update && sudo apt-get upgrade"
-
-# lazygit
+alias _ip="curl ifconfig.me"
+alias nvenv="python -m venv .venv"
+alias k=kubectl
 alias g=lazygit
 alias cat=bat
 alias greset="git reset --soft HEAD^"
